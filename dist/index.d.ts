7f520e039f79f7340769d80042c584d0aa59fd8b
Fabio Spampinato <spampinabio@gmail.com>
2017-03-02 17:30:04 +0100

    Bumped version to 1.0.1

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,8 +1,9 @@
 declare const Builder: {
-    make(what: "query" | "mutation" | "subscription", name: string, string?: boolean): any;
-    query(name: string, string?: boolean): any;
-    mutation(name: string, string?: boolean): any;
-    _toString(what: string, type: string, name: string, resolver: any): string;
+    make(what: "query" | "mutation" | "subscription", resolver: string, string?: boolean): any;
+    query(resolver: string, string?: boolean): any;
+    mutation(resolver: string, string?: boolean): any;
+    subscription(resolver: string, string?: boolean): any;
+    _toString(what: string, type: string, resolver: string, data: any): string;
     _toGQL(string: any): any;
     _expandType(type: any): any;
     _getFields(type: any): string;
